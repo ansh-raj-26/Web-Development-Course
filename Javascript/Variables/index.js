c0d0a6dc117d9740401a0f86ae267abe7a6244d0
ansh-raj-26 <anshr948@gmail.com>
2026-05-25 23:10:14 +0530

    Learned Variables

---
+++ Javascript/Variables/index.js
@@ -0,0 +1,43 @@
+
+// format for declaring the variables:
+
+//declaration let x;
+//assignemnt x= 100;
+
+
+/*let age = 20;
+let income = 1.5;
+let cgpa=7.7;
+
+console.log(typeof cgpa)*/ /* this tell the datatype of the variable*/
+/*console.log(`My age is ${age}`);
+console.log(`You montlly income is ${income}`);
+console.log(`Your cgpa is ${cgpa}`);*/
+
+
+//Strings
+
+/*let firstName = "Ansh";
+let gmail = "ansh948@gmail.com";
+
+console.log(`My firstname is ${firstName}`);
+console.log(`The gmail is ${gmail} for contacting me`);*/
+
+// Bollean:
+
+/*let online = true;
+let isstudent = false;
+
+console.log(`Is the website online ${online}`)
+console.log(`Enrolled: ${isstudent}`)*/
+
+// How to print in the web page:
+
+let fullName = "Ansh Raj";
+let age = 20;
+let isStudent = true;
+
+document.getElementById("p1").textContent=`My fullname is ${fullName}`;
+document.getElementById("p2").textContent=`I am ${age} years old`;
+document.getElementById("p3").textContent=`Enrolled: ${isStudent}`;
+
---
+++ Javascript/script.js
@@ -0,0 +1,7 @@
+ /* used to print the variable etc
+ console.log('Hello')*/
+ 
+ // display an alert window.alert('I am an alert')
+
+ document.getElementById('myh1').textContent='What is this?'
+ document.getElementById('myp').textContent='this is a paragraph'
