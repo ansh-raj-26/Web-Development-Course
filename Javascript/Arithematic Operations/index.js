4e7c9498c974c5b5feec41c576c61eab72c55b6f
ansh-raj-26 <anshr948@gmail.com>
2026-05-27 21:46:45 +0530

    Learned Operators

---
+++ Javascript/Arithematic Operations/index.js
@@ -0,0 +1,29 @@
+
+// Arithematic Operator: operators(+, -, *, /)
+
+let students = 30;
+
+/*students += 1;
+students -=1
+students *= 2
+students /= 2
+students **=2
+students %=2 */
+
+
+
+
+//students++ (Its incementations +1)
+//students-- (Its decremenatation -1)
+
+
+/*    The Rule (Similar to BODMAS):
+    Parenthesis
+    exponents
+    multiplication & division & modulo
+    addition & subtraction
+*/
+
+let result =1+2*3+4**2/2;
+
+console.log(result);
