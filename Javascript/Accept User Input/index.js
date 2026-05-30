cde7575948ddf995f47aedfd3611e68f05d63fe6
ansh-raj-26 <anshr948@gmail.com>
2026-05-30 12:44:18 +0530

    Got to know how to accept userinput

--- Javascript/Accept User Input/index.js
+++ Javascript/Accept User Input/index.js
@@ -1,1 +1,35 @@
-git 
+// How to accept user input:
+
+// Two Ways:
+//   Window Prompt
+//   HTML Textbox
+
+/*let username;
+
+username = window.prompt("What is you'r username");
+
+console.log(username)  */ 
+
+
+let username;
+
+document.getElementById("mySubmit").onclick = function(){
+   username = document.getElementById("myText").value;
+   document.getElementById("myH1").textContent=`Hello ${username}`;
+}
+//This is HTML Form
+
+//Notes:
+
+
+/* User types text
+        ↓
+Input Box (.value)
+        ↓
+Store in variable
+        ↓
+Button Click
+        ↓
+Update H1 (.textContent)
+        ↓
+Display Output */
