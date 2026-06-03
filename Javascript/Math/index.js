ffc6bd9e4aded79c9ea748d75deb7b27b1555d8e
ansh-raj-26 <anshr948@gmail.com>
2026-06-03 14:20:41 +0530

    Math object in JS

---
+++ Javascript/Math/index.js
@@ -0,0 +1,22 @@
+//Math: A built in object that provide collection of properties and methods
+
+let x = 10;
+let y = 3;
+let z = 9;
+
+// z = Math.round(x) round off the values to the closes number
+// z = Math.floor(x) ingnore the decimal places 
+// z = Math.ceil(x) it will increase to teh next digit 
+// z = Math.trunc(x); this doenot roudoff to any number i will be same as the input just will not show the decimal numbers
+// z = Math.pow(x, y) its the x to the power of y like 2 to power of 3
+// z = Math.sqrt(x) square root of the x.
+// z = Math.log(x)
+// z = Math.sin(x)
+// z = Math.cos(x)
+// z = Math.tan(x)
+// z = Math.abs(x) just convert the -ve to positive
+// z = Math.sign(x)
+// let max =Math.max(x, y, z) give the max no 
+// let min = Math.min(x, y, z) give the min no 
+
+console.log(min)
